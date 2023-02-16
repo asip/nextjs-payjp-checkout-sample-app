c2e5421bec44131786af0871bab1b2163f27374e
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-16 10:48:54 +0900

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import PayjpCheckout from '@/components/payjp_checkout'
 
 export default function Call(props) {
     const payjpCheckoutProps = {
-        dataKey: 'pk_test_8b4b4c47d9cc18327a0c1869',
+        dataKey: '',
         dataText: 'クレジットカードで支払う',
         dataPartial: 'true',
         onCreatedHandler: onCreated,
