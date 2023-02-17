f26df9310a0d8b1f7ddf2f699e2b50fac2014728
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-17 11:21:14 +0900

    refactor: edit components/payjp_checkout

--- components/payjp_checkout.js
+++ components/payjp_checkout.js
@@ -15,9 +15,9 @@ class PayjpCheckout extends React.Component {
 
     componentDidMount() {
         this.windowAlertBackUp = window.alert;
-        window.reactPayjpCheckoutOnCreated = this.onCreated;
-        window.reactPayjpCheckoutOnFailed = this.onFailed;
-        window.reactPayjpCheckoutContext = this;
+        window.payjpCheckoutOnCreated = this.onCreated;
+        window.payjpCheckoutOnFailed = this.onFailed;
+        window.payjpCheckoutContext = this;
         // カード情報が不正のときに window.alert が payjp の checkout から呼ばれるため
         window.alert = () => {};
 
@@ -31,8 +31,8 @@ class PayjpCheckout extends React.Component {
         this.props.dataTokenName && (this.script.dataset['tokenName'] = this.props.dataTokenName);
         this.props.dataPreviousToken && (this.script.dataset['previousToken'] = this.props.dataPreviousToken);
         this.props.dataLang && (this.script.dataset['lang'] = this.props.dataLang);
-        this.script.dataset['onCreated'] = 'reactPayjpCheckoutOnCreated';
-        this.script.dataset['onFailed'] = 'reactPayjpCheckoutOnFailed';
+        this.script.dataset['onCreated'] = 'payjpCheckoutOnCreated';
+        this.script.dataset['onFailed'] = 'payjpCheckoutOnFailed';
         this.props.dataNamePlaceholder && (this.script.dataset['namePlaceholder'] = this.props.dataNamePlaceholder);
         this.payjpCheckoutElement = document.getElementById('payjpCheckout');
         this.payjpCheckoutElement && this.payjpCheckoutElement.appendChild(this.script);
@@ -41,9 +41,9 @@ class PayjpCheckout extends React.Component {
     componentWillUnmount() {
         // すでに https://checkout.pay.jp/ の checkout.js が実行済みで、script タグを削除しているだけ
         this.payjpCheckoutElement.removeChild(this.script);
-        window.reactPayjpCheckoutOnCreated = null;
-        window.reactPayjpCheckoutOnFailed = null;
-        window.reactPayjpCheckoutContext = null;
+        window.payjpCheckoutOnCreated = null;
+        window.payjpCheckoutOnFailed = null;
+        window.payjpCheckoutContext = null;
         window.alert = this.windowAlertBackUp;
         window.PayjpCheckout = null;
     }
@@ -54,12 +54,12 @@ class PayjpCheckout extends React.Component {
 
     onCreated(response) {
         const payload = { token: response.id }
-        window.reactPayjpCheckoutContext.props.onCreatedHandler(payload);
+        window.payjpCheckoutContext.props.onCreatedHandler(payload);
     }
 
     onFailed(statusCode, errorResponse) {
         const payload = { message: errorResponse.message }
-        window.reactPayjpCheckoutContext.props.onFailedHandler(payload);
+        window.payjpCheckoutContext.props.onFailedHandler(payload);
     }
 
     render() {
