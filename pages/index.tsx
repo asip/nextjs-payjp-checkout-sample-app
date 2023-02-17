70dc13bdd4193ad2bb2e9a991e6b695da2551379
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-17 19:57:14 +0900

    style:  edit pages/index & components/payjp_checkout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,26 @@ import React from 'react'
 import PayjpCheckout from '@/components/payjp_checkout'
 
 export default function Call() {
-    const payjpCheckoutProps = {
-        dataKey: '',
-        dataText: 'クレジットカードで支払う',
-        dataPartial: 'true',
-        onCreatedHandler: onCreated,
-        onFailedHandler: onFailed,
-    }
+  const payjpCheckoutProps = {
+    dataKey: '',
+    dataText: 'クレジットカードで支払う',
+    dataPartial: 'true',
+    onCreatedHandler: onCreated,
+    onFailedHandler: onFailed,
+  }
 
-    function onCreated(payload: any){
-        //console.log(payload)
-        console.log(payload.token)
-    }
+  function onCreated(payload: any) {
+    //console.log(payload)
+    console.log(payload.token)
+  }
 
-    function onFailed(payload: any){
-        console.log(payload.message)
-    }
+  function onFailed(payload: any) {
+    console.log(payload.message)
+  }
 
-    return (
-        <div className="payjpButtonArea">
-            <PayjpCheckout {...payjpCheckoutProps} />
-        </div>
-    )
+  return (
+    <div className="payjpButtonArea">
+      <PayjpCheckout {...payjpCheckoutProps} />
+    </div>
+  )
 }
